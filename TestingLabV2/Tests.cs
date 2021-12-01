5cd98c97ec34aca10f0c5f695682ae346c88e472
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2021-12-02 01:33:28 +0300

    Добавьте файлы проекта.

---
+++ TestingLabV2/Tests.cs
@@ -0,0 +1,128 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLab3V2
+{
+	[TestFixture]
+	public class Tests
+	{
+		//[Test]
+		//public void IsCorrectTest()
+		//      {
+		//	Assert.Throws<ArgumentException>(() =>
+		//	{
+		//		ArgumentException("Значение версии не может быть распознано корректно");
+		//	});
+		//}
+		[Test]
+		public void MoreTest()
+		{
+			Assert.Throws<ArgumentException>(() =>
+			{
+				_ = new Versions("1.0,0-beta.alpha") > new Versions("1.0.0-beta.1");
+			});
+
+			Assert.Throws<ArgumentException>(() =>
+			{
+				_ = new Versions("QWERTY") > new Versions("1.0.0-beta.1");
+			});
+
+			Assert.Throws<ArgumentException>(() =>
+			{
+				Versions version = new Versions("10.5.1-aplha.beta");
+			});
+
+			Assert.IsTrue(new Versions("1.0.0-alpha.1") > new Versions("1.0.0-alpha.beta"));
+			Assert.IsTrue(new Versions("1.0.0") > new Versions("1.0.0-alpha"));
+			Assert.IsTrue(new Versions("1.0.1") > new Versions("1.0.0"));
+			Assert.IsTrue(new Versions("1.1.0") > new Versions("1.0.0"));
+			Assert.IsTrue(new Versions("1.1.1") > new Versions("1.1.0"));
+
+			Assert.IsFalse(new Versions("0.0.9-alpha.5") > new Versions("0.10.0-beta.10.5"));
+			Assert.IsFalse(new Versions("4.1.9-1") > new Versions("5.9.0-5"));
+			Assert.IsFalse(new Versions("7.1.4-1") > new Versions("7.1.5-1"));
+			Assert.IsFalse(new Versions("0.1.9-1") > new Versions("0.2.0-5"));
+		}
+
+		[Test]
+		public void LessTest()
+		{
+			Assert.Throws<ArgumentException>(() =>
+			{
+				_ = new Versions("?.!,0-beta.alpha") > new Versions("1.0.0-alpha.1");
+			});
+
+			Assert.IsTrue(new Versions("1.0.0-beta.alpha") < new Versions("1.0.0-beta.1"));
+			Assert.IsTrue(new Versions("5.5.4-beta.1") < new Versions("5.5.5-beta.alpha"));
+			Assert.IsTrue(new Versions("0.1.1-beta.1") < new Versions("0.1.2-beta.alpha"));
+
+			Assert.IsFalse(new Versions("54.48.89") < new Versions("54.48.80-alpha"));
+			Assert.IsFalse(new Versions("8.14.16") < new Versions("8.14.15"));
+			Assert.IsFalse(new Versions("1.0.8-beta.1") < new Versions("1.0.8-beta.0"));
+		}
+
+		[Test]
+		public void MoreOrEqualTest()
+		{
+			Assert.IsTrue(new Versions("1.1.0") >= new Versions("1.0.0"));
+			Assert.IsTrue(new Versions("4.6.7") >= new Versions("4.6.0"));
+			Assert.IsTrue(new Versions("5.5.5") >= new Versions("5.5.5-alpha"));
+
+			Assert.IsFalse(new Versions("5.10.45") >= new Versions("5.10.46"));
+			Assert.IsFalse(new Versions("40.50.40") >= new Versions("40.50.45"));
+			Assert.IsFalse(new Versions("10.1.5-alpha.54.beta") >= new Versions("10.1.5-alpha.54.1"));
+		}
+
+		[Test]
+		public void LessOrEqualTest()
+		{
+			Assert.IsTrue(new Versions("1.0.0") <= new Versions("1.1.0"));
+			Assert.IsTrue(new Versions("4.8.0") <= new Versions("4.9.1"));
+			Assert.IsTrue(new Versions("40.40.40-alpha") <= new Versions("40.40.40"));
+			Assert.IsTrue(new Versions("0.1.0-alpha") <= new Versions("0.1.0-alpha"));
+
+			Assert.IsFalse(new Versions("5.8.1-alpha") <= new Versions("4.8.7-alpha"));
+			Assert.IsFalse(new Versions("4.1.7-alpha.5") <= new Versions("4.1.7-alpha.1"));
+			Assert.IsFalse(new Versions("50.80.1-1") <= new Versions("50.80.1-alpha")); // here
+		}
+
+		[Test]
+		public void EqualTest()
+		{
+			Assert.IsTrue(new Versions("0.0.0") == new Versions("0.0.0"));
+			Assert.IsTrue(new Versions("5.1.1") == new Versions("5.1.1"));
+			Assert.IsTrue(new Versions("1.1.1-beta") == new Versions("1.1.1-beta"));
+
+			Assert.IsFalse(new Versions("50.10.8") == new Versions("50.10.9"));
+			Assert.IsFalse(new Versions("40.88.99-alpha") == new Versions("40.88.99-alpha.5"));
+			Assert.IsFalse(new Versions("80.10.7-alpha") == new Versions("80.10.7"));
+		}
+
+		[Test]
+		public void NoEqualTest()
+		{
+			Assert.Throws<ArgumentException>(() =>
+			{
+				bool metka = new Versions("1.0,0-beta.alpha") > new Versions("qwerty");
+			});
+
+			Assert.IsTrue(new Versions("1.0.0") != new Versions("1.0.1"));
+			Assert.IsTrue(new Versions("5.1.5-alpha") != new Versions("5.1.5"));
+			Assert.IsTrue(new Versions("10.44.1-alpha.beta") != new Versions("10.44.1-alpha.beta.1"));
+			Assert.IsFalse(new Versions("1.0.0") != new Versions("1.0.0"));
+			Assert.IsFalse(new Versions("8.7.4-1") != new Versions("8.7.4-1"));
+		}
+
+		[Test]
+		public void ToStringTest()
+		{
+			Assert.AreEqual("1.0.0", new Versions("1.0.0").ToString());
+			Assert.AreEqual("1.1.1-alpha", new Versions("1.1.1-alpha").ToString());
+			Assert.AreEqual("10.50.1-alpha.beta.1", new Versions("10.50.1-alpha.beta.1").ToString());
+		}
+	}
+}
---
+++ TestingLabV2/Versions.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLab3V2
+{
+	public class Versions
+	{
+		public int Unit_1_Part_1 { get; set; }
+		public int Unit_1_Part_2 { get; set; }
+		public int Unit_1_Part_3 { get; set; }
+		public int? Unit_2_Part_1 { get; set; }
+		public int? Unit_2_Part_2 { get; set; }
+		public int? Unit_2_Part_3 { get; set; }
+
+		public Versions(string Versions)
+		{
+			if (IsCorrect(Versions))
+            {
+				string[] splitedUnits = Versions.Split('-');
+				string[] splitedPartsOfUnit1 = splitedUnits[0].Split('.');
+				Unit_1_Part_1 = Convert.ToInt32(splitedPartsOfUnit1[0]);
+				Unit_1_Part_2 = Convert.ToInt32(splitedPartsOfUnit1[1]);
+				Unit_1_Part_3 = Convert.ToInt32(splitedPartsOfUnit1[2]);
+
+				if (splitedUnits.Length > 1)
+				{
+					string[] splitedPartsOfUnit2 = splitedUnits[1].Split('.');
+					switch (splitedPartsOfUnit2.Length)
+					{
+						case 1:
+							Unit_2_Part_1 = ConvertPartToDigit(splitedPartsOfUnit2[0]);
+							Unit_2_Part_2 = null;
+							Unit_2_Part_3 = null;
+							break;
+						case 2:
+							Unit_2_Part_1 = ConvertPartToDigit(splitedPartsOfUnit2[0]);
+							Unit_2_Part_2 = ConvertPartToDigit(splitedPartsOfUnit2[1]);
+							Unit_2_Part_3 = null;
+							break;
+						case 3:
+							Unit_2_Part_1 = ConvertPartToDigit(splitedPartsOfUnit2[0]);
+							Unit_2_Part_2 = ConvertPartToDigit(splitedPartsOfUnit2[1]);
+							Unit_2_Part_3 = ConvertPartToDigit(splitedPartsOfUnit2[2]);
+							break;
+					}
+				}
+            }
+			else
+            {
+				throw new ArgumentException("Недопустимый формат версии");
+			}
+		}
+
+		private static int ConvertPartToDigit(string part)
+		{
+			if (part == "alpha")
+			{
+				return -2;
+			}
+			if (part == "beta")
+			{
+				return -1;
+			}
+			return Convert.ToInt32(part);
+		}
+
+		private static string ConvertDigitToPart(int? digit)
+		{
+			if (digit == -2)
+			{
+				return "alpha";
+			}
+			if (digit == -1)
+			{
+				return "beta";
+			}
+			return digit.ToString();
+		}
+
+
+		private static bool IsCorrect(string Versions)
+		{
+			string[] splitedByDashVersion = Versions.Split('-');
+			if (splitedByDashVersion.Length == 1)
+			{
+				int n = 0;
+				string[] splitedByDotVersion = Versions.Split('.');
+				if (splitedByDotVersion.Length == 3)
+				{
+					if (Int32.TryParse(splitedByDotVersion[0], out n) == true)
+					{
+						if (Int32.TryParse(splitedByDotVersion[1], out n) == true)
+						{
+							if (Int32.TryParse(splitedByDotVersion[2], out n) == true)
+							{
+								return true;
+							}
+						}
+					}
+				}
+				return false;
+			}
+			if (splitedByDashVersion.Length == 2)
+			{
+				bool flagOfPt1 = false;
+				bool flagOfPt2 = false;
+				int n = 0;
+				string[] splitedByDotVersionPt1 = splitedByDashVersion[0].Split('.');
+				if (splitedByDotVersionPt1.Length == 3)
+				{
+					if (Int32.TryParse(splitedByDotVersionPt1[0], out n) == true)
+					{
+						if (Int32.TryParse(splitedByDotVersionPt1[1], out n) == true)
+						{
+							if (Int32.TryParse(splitedByDotVersionPt1[2], out n) == true)
+							{
+								flagOfPt1 = true;
+							}
+						}
+					}
+				}
+				string[] splitedByDotVersionPt2 = splitedByDashVersion[1].Split('.');
+				if (splitedByDotVersionPt2.Length <= 3)
+				{
+					int tryParseResult = 0;
+					switch (splitedByDotVersionPt2.Length)
+					{
+						case 1:
+							if (splitedByDotVersionPt2[0].Length == 1 || splitedByDotVersionPt2[0] == "alpha" || splitedByDotVersionPt2[0] == "beta" || Int32.TryParse(splitedByDotVersionPt2[0], out tryParseResult))
+							{
+								flagOfPt2 = true;
+							}
+							break;
+						case 2:
+							if (splitedByDotVersionPt2[0].Length == 1 || splitedByDotVersionPt2[0] == "alpha" || splitedByDotVersionPt2[0] == "beta" || Int32.TryParse(splitedByDotVersionPt2[0], out tryParseResult))
+							{
+								if (splitedByDotVersionPt2[1].Length == 1 || splitedByDotVersionPt2[1] == "alpha" || splitedByDotVersionPt2[1] == "beta" || Int32.TryParse(splitedByDotVersionPt2[1], out tryParseResult))
+								{
+									flagOfPt2 = true;
+								}
+							}
+							break;
+						case 3:
+							if (splitedByDotVersionPt2[0].Length == 1 || splitedByDotVersionPt2[0] == "alpha" || splitedByDotVersionPt2[0] == "beta" || Int32.TryParse(splitedByDotVersionPt2[0], out tryParseResult))
+							{
+								if (splitedByDotVersionPt2[1].Length == 1 || splitedByDotVersionPt2[1] == "alpha" || splitedByDotVersionPt2[1] == "beta" || Int32.TryParse(splitedByDotVersionPt2[1], out tryParseResult))
+								{
+									if (splitedByDotVersionPt2[2].Length == 1 || splitedByDotVersionPt2[2] == "alpha" || splitedByDotVersionPt2[2] == "beta" || Int32.TryParse(splitedByDotVersionPt2[2], out tryParseResult))
+									{
+										flagOfPt2 = true;
+									}
+								}
+							}
+							break;
+					}
+				}
+				if (flagOfPt1 == true && flagOfPt2 == true)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static bool operator >(Versions version1, Versions version2)
+		{
+			return IsMore(version1, version2);
+		}
+		public static bool operator <(Versions version1, Versions version2)
+		{
+			return !IsMore(version1, version2);
+		}
+
+		private static bool IsMore(Versions v1, Versions v2)
+		{
+			if (v1.Unit_1_Part_1 > v2.Unit_1_Part_1)
+			{
+				return true;
+			}
+			else if (v1.Unit_1_Part_1 == v2.Unit_1_Part_1)
+			{
+				if (v1.Unit_1_Part_2 > v2.Unit_1_Part_2)
+				{
+					return true;
+				}
+				else if (v1.Unit_1_Part_2 == v2.Unit_1_Part_2)
+				{
+					if (v1.Unit_1_Part_3 > v2.Unit_1_Part_3)
+					{
+						return true;
+					}
+					else if (v1.Unit_1_Part_3 == v2.Unit_1_Part_3)
+					{
+						if (v1.Unit_2_Part_1 > v2.Unit_2_Part_1)
+						{
+							return true;
+						}
+						if (v1.Unit_2_Part_1 == null && v2.Unit_2_Part_1 != null)
+						{
+							return true;
+						}
+						if (v1.Unit_2_Part_1 == v2.Unit_2_Part_1)
+						{
+							if (v1.Unit_2_Part_2 > v2.Unit_2_Part_2)
+							{
+								return true;
+							}
+							if (v1.Unit_2_Part_2 == null && v2.Unit_2_Part_2 != null)
+							{
+								return true;
+							}
+							if (v1.Unit_2_Part_2 == v2.Unit_2_Part_2)
+							{
+								if (v1.Unit_2_Part_3 > v2.Unit_2_Part_3)
+								{
+									return true;
+								}
+								if (v1.Unit_2_Part_3 == null && v2.Unit_2_Part_3 != null)
+								{
+									return true;
+								}
+							}
+						}
+					}
+				}
+			}
+			return false;
+		}
+
+		public static bool operator >=(Versions version1, Versions version2)
+		{
+			return IsMoreOrEqual(version1, version2);
+		}
+
+		private static bool IsMoreOrEqual(Versions v1, Versions v2)
+		{
+			if (v1.Unit_1_Part_1 > v2.Unit_1_Part_1)
+			{
+				return true;
+			}
+			else if (v1.Unit_1_Part_1 == v2.Unit_1_Part_1)
+			{
+				if (v1.Unit_1_Part_2 > v2.Unit_1_Part_2)
+				{
+					return true;
+				}
+				else if (v1.Unit_1_Part_2 == v2.Unit_1_Part_2)
+				{
+					if (v1.Unit_1_Part_3 > v2.Unit_1_Part_3)
+					{
+						return true;
+					}
+					else if (v1.Unit_1_Part_3 == v2.Unit_1_Part_3)
+					{
+						if (v1.Unit_2_Part_1 > v2.Unit_2_Part_1)
+						{
+							return true;
+						}
+						if (v1.Unit_2_Part_1 == null && v2.Unit_2_Part_1 != null)
+						{
+							return true;
+						}
+						if (v1.Unit_2_Part_1 == v2.Unit_2_Part_1)
+						{
+							if (v1.Unit_2_Part_2 > v2.Unit_2_Part_2)
+							{
+								return true;
+							}
+							if (v1.Unit_2_Part_2 == null && v2.Unit_2_Part_2 != null)
+							{
+								return true;
+							}
+							if (v1.Unit_2_Part_2 == v2.Unit_2_Part_2)
+							{
+								if (v1.Unit_2_Part_3 > v2.Unit_2_Part_3)
+								{
+									return true;
+								}
+								if (v1.Unit_2_Part_3 == null && v2.Unit_2_Part_3 != null)
+								{
+									return true;
+								}
+								if (v1.Unit_2_Part_3 == v2.Unit_2_Part_3)
+								{
+									return true;
+								}
+							}
+						}
+					}
+				}
+			}
+			return false;
+		}
+
+		public static bool operator <=(Versions version1, Versions version2)
+		{
+			return IsLessOrEqual(version1, version2);
+		}
+
+		private static bool IsLessOrEqual(Versions v1, Versions v2)
+		{
+			if (v1.Unit_1_Part_1 > v2.Unit_1_Part_1)
+			{
+				return false;
+			}
+			else if (v1.Unit_1_Part_1 == v2.Unit_1_Part_1)
+			{
+				if (v1.Unit_1_Part_2 > v2.Unit_1_Part_2)
+				{
+					return false;
+				}
+				else if (v1.Unit_1_Part_2 == v2.Unit_1_Part_2)
+				{
+					if (v1.Unit_1_Part_3 > v2.Unit_1_Part_3)
+					{
+						return false;
+					}
+					else if (v1.Unit_1_Part_3 == v2.Unit_1_Part_3)
+					{
+						if (v1.Unit_2_Part_1 > v2.Unit_2_Part_1)
+						{
+							return false;
+						}
+						if (v1.Unit_2_Part_1 == null && v2.Unit_2_Part_1 != null)
+						{
+							return false;
+						}
+						if (v1.Unit_2_Part_1 == v2.Unit_2_Part_1)
+						{
+							if (v1.Unit_2_Part_2 > v2.Unit_2_Part_2)
+							{
+								return false;
+							}
+							if (v1.Unit_2_Part_2 == null && v2.Unit_2_Part_2 != null)
+							{
+								return false;
+							}
+							if (v1.Unit_2_Part_2 == v2.Unit_2_Part_2)
+							{
+								if (v1.Unit_2_Part_3 > v2.Unit_2_Part_3)
+								{
+									return false;
+								}
+								if (v1.Unit_2_Part_3 == null && v2.Unit_2_Part_3 != null)
+								{
+									return false;
+								}
+								if (v1.Unit_2_Part_3 == v2.Unit_2_Part_3)
+								{
+									return true;
+								}
+							}
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		public static bool operator ==(Versions version1, Versions version2)
+		{
+			return IsEqual(version1, version2);
+		}
+
+		public static bool operator !=(Versions version1, Versions version2)
+		{
+			return !IsEqual(version1, version2);
+		}
+
+		private static bool IsEqual(Versions v1, Versions v2)
+		{
+			if (v1.ToString() == v2.ToString())
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public override string ToString()
+		{
+			if (Unit_2_Part_1 != null && Unit_2_Part_2 != null && Unit_2_Part_3 != null)
+			{
+				return $"{Unit_1_Part_1}.{Unit_1_Part_2}.{Unit_1_Part_3}-{ConvertDigitToPart(Unit_2_Part_1)}.{ConvertDigitToPart(Unit_2_Part_2)}.{ConvertDigitToPart(Unit_2_Part_3)}";
+			}
+			if (Unit_2_Part_1 != null && Unit_2_Part_2 != null && Unit_2_Part_3 == null)
+			{
+				return $"{Unit_1_Part_1}.{Unit_1_Part_2}.{Unit_1_Part_3}-{ConvertDigitToPart(Unit_2_Part_1)}.{ConvertDigitToPart(Unit_2_Part_2)}";
+			}
+			if (Unit_2_Part_1 != null && Unit_2_Part_2 == null && Unit_2_Part_3 == null)
+			{
+				return $"{Unit_1_Part_1}.{Unit_1_Part_2}.{Unit_1_Part_3}-{ConvertDigitToPart(Unit_2_Part_1)}";
+			}
+			if (Unit_2_Part_1 == null && Unit_2_Part_2 == null && Unit_2_Part_3 == null)
+			{
+				return $"{Unit_1_Part_1}.{Unit_1_Part_2}.{Unit_1_Part_3}";
+			}
+			return "";
+		}
+	}
+}
+
